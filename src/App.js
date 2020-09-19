fa739ababb623e43e849b1527413aceeb6bca052
mahi-kabir <mahi.kabir@yahoo.com>
2020-09-19 21:21:26 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const heroes = ['ironman', 'spiderman','batman'];
+
+  const villans = ['thanos', 'rhino', 'joker'];
+
+  const contests = [
+    {hero: 'ironman', villan: 'thanos', winner: 'ironman'},
+    {hero: 'spiderman', villan: 'rhino', winner: 'spiderman'},
+    {hero: 'batman', villan: 'joker', winner: 'batman'},
+    {hero: 'captain america', villan: 'baki vai',winner:''}
+  ];
+
+  
+
+  
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      {
+        heroes.map(hero => <li> {hero} </li>)
+      }
+    
+    {
+      villans.map(villan => <li>{villan}</li>)
+    }
+
+
+    {
+      contests.map(contest => <li>{contest.hero}</li>)
+    }
+
+    {
+      contests.map(contest => <Fights hr = {contest.hero} vl = {contest.villan}></Fights>)
+    }
+
+
       </header>
     </div>
   );
 }
+
+
+function Fights(props){
+
+  return(
+    <div>
+    <h2>I am {props.hr} will play </h2>
+    <h4>vs {props.vl}</h4>
+    
+
+  </div>
+      
+  )
+}
+
+
 
 export default App;
